75d59743610f13e12d9c68fd01ed5c583f754dc4
muaddibttv <31803191+muaddibttv@users.noreply.github.com>
2018-04-08 00:26:14 -0400

    Converted over to requests.get() for Python 3
    urllib2 removed, didn't know so converting over.

--- Jen Generators/jen_ytplaylist_parser.py
+++ Jen Generators/jen_ytplaylist_parser.py
@@ -1,7 +1,7 @@
 # -*- coding: UTF-8 -*-
 """ Processes HTML of a YT channel's Playlists, to build a template file for all of them. """
 
-import datetime, json, urllib2, os, re, sys, traceback
+import datetime, json, os, re, requests, sys, traceback
 
 YOUTUBE_API_KEY = "AIzaSyA4ktCh7tLBk467AYgPMykgdtMZ8HL68hE"
 RESULTS_PER_PAGE = '40' # 1-50 as per Google's rules.
@@ -36,8 +36,8 @@ class Generator:
             cycle = 0
             url = first_url + '&channelId='+self.channel+'&part=id,snippet,contentDetails'
 
-            inp = urllib2.urlopen(url)
-            resp = json.load(inp)
+            inp = requests.get(url, headers={'User-Agent':randomagent()})
+            resp = inp.json()
             playlist_count = len(resp["items"])
             playlist_worked = 1;
             for playlist in resp['items']:
@@ -90,6 +90,27 @@ def replaceEscapeCodes(txt):
         import HTMLParser as html_parser
     txt = html_parser.HTMLParser().unescape(txt)
     return txt
+
+def randomagent():
+    import random
+    BR_VERS = [
+        ['%s.0' % i for i in range(18, 50)],
+        ['37.0.2062.103', '37.0.2062.120', '37.0.2062.124', '38.0.2125.101', '38.0.2125.104', '38.0.2125.111', '39.0.2171.71', '39.0.2171.95', '39.0.2171.99',
+         '40.0.2214.93', '40.0.2214.111',
+         '40.0.2214.115', '42.0.2311.90', '42.0.2311.135', '42.0.2311.152', '43.0.2357.81', '43.0.2357.124', '44.0.2403.155', '44.0.2403.157', '45.0.2454.101',
+         '45.0.2454.85', '46.0.2490.71',
+         '46.0.2490.80', '46.0.2490.86', '47.0.2526.73', '47.0.2526.80', '48.0.2564.116', '49.0.2623.112', '50.0.2661.86', '51.0.2704.103', '52.0.2743.116',
+         '53.0.2785.143', '54.0.2840.71', '61.0.3163.100'],
+        ['11.0'],
+        ['8.0', '9.0', '10.0', '10.6']]
+    WIN_VERS = ['Windows NT 10.0', 'Windows NT 7.0', 'Windows NT 6.3', 'Windows NT 6.2', 'Windows NT 6.1', 'Windows NT 6.0', 'Windows NT 5.1', 'Windows NT 5.0']
+    FEATURES = ['; WOW64', '; Win64; IA64', '; Win64; x64', '']
+    RAND_UAS = ['Mozilla/5.0 ({win_ver}{feature}; rv:{br_ver}) Gecko/20100101 Firefox/{br_ver}',
+                'Mozilla/5.0 ({win_ver}{feature}) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/{br_ver} Safari/537.36',
+                'Mozilla/5.0 ({win_ver}{feature}; Trident/7.0; rv:{br_ver}) like Gecko',
+                'Mozilla/5.0 (compatible; MSIE {br_ver}; {win_ver}{feature}; Trident/6.0)']
+    index = random.randrange(len(RAND_UAS))
+    return RAND_UAS[index].format(win_ver=random.choice(WIN_VERS), feature=random.choice(FEATURES), br_ver=random.choice(BR_VERS[index]))
 
 if ( __name__ == "__main__" ):
     # start
