e52e0ca0744472ea562bf52f98794f3132fd3f81
muaddibttv <31803191+muaddibttv@users.noreply.github.com>
2018-11-17 12:05:11 -0500

    SwiftStreamz Update

--- !Jen Template/Additional Plugins/swift.py
+++ !Jen Template/Additional Plugins/swift.py
@@ -6,37 +6,24 @@
 
     ----------------------------------------------------------------------------
     "THE BEER-WARE LICENSE" (Revision 42):
-    @tantrumdev wrote this file.  As long as you retain this notice you can do 
+    @tantrumdev wrote this file.  As long as you retain this notice you can do
     whatever you want with this stuff. Just Ask first when not released through
     the tools and parser GIT. If we meet some day, and you think this stuff is
     worth it, you can buy him a beer in return. - Muad'Dib
     ----------------------------------------------------------------------------
 
 
-    Examples:
-        <dir>
-            <title>USA Live TV</title>
-            <meta>
-                <summary>Live United States TV Feeds</summary>
-            </meta>
-            <swift>swiftcategory/41</swift>
-        </dir>
-
-        <dir>
-            <title>All TV Categories</title>
-            <meta>
-                <summary>All Live TV Categories for various countries and themes</summary>
-            </meta>
-            <swift>allthespice</swift>
-        </dir>
-
-
 """
 
-import base64,json,re,requests,os,traceback,urlparse
-import koding
 import __builtin__
-import xbmc,xbmcaddon,xbmcgui
+
+import requests
+# import traceback
+import koding
+import xbmc
+import xbmcaddon
+import xbmcgui
+
 from koding import route
 from resources.lib.plugin import Plugin
 from resources.lib.util import dom_parser
@@ -47,8 +34,8 @@ from unidecode import unidecode
 CACHE_TIME = 3600  # change to wanted cache time in seconds
 
 addon_fanart = xbmcaddon.Addon().getAddonInfo('fanart')
-addon_icon   = xbmcaddon.Addon().getAddonInfo('icon')
-User_Agent   = 'Dalvik/2.1.0 (Linux; U; Android 5.1.1; AFTS Build/LVY48F)'
+addon_icon = xbmcaddon.Addon().getAddonInfo('icon')
+User_Agent = 'Dalvik/2.1.0 (Linux; U; Android 5.1.1; AFTS Build/LVY48F)'
 
 base_api_url = 'http://swiftstreamz.com/SwiftLive/api.php'
 base_dta_url = 'http://swiftstreamz.com/SwiftLive/swiftlive.php'
@@ -123,10 +110,10 @@ class SwiftLive(Plugin):
 @route(mode='SwiftMain', args=["url"])
 def get_swiftstreamz(url):
     xml = ""
-    url = url.replace('allthespice/', '') # Strip our category tag off.
+    url = url.replace('allthespice/', '')  # Strip our category tag off.
     try:
         headers = {'Authorization': 'Basic U3dpZnRTdHJlYW16OkBTd2lmdFN0cmVhbXpA', 'User-Agent': User_Agent}
-        response = requests.get(base_api_url,headers=headers)
+        response = requests.get(base_api_url, headers=headers)
         if 'Erreur 503' in response.content:
             xml += "<dir>"\
                    "    <title>[B]System down for maintenance[/B]</title>"\
@@ -138,10 +125,11 @@ def get_swiftstreamz(url):
                    "</dir>" % (addon_icon)
         else:
             response = response.json(strict=False)
+            # xbmcgui.Dialog().textviewer('Whatevs', str(response))
             for a in response['LIVETV']:
                 try:
                     name = a['category_name']
-                    id   = a['cid']
+                    id = a['cid']
                     icon = base_ico_url % (a['category_image'])
                     xml += "<dir>"\
                            "    <title>%s</title>"\
@@ -150,10 +138,10 @@ def get_swiftstreamz(url):
                            "    </meta>"\
                            "    <swift>swiftcategory/%s</swift>"\
                            "    <thumbnail>%s</thumbnail>"\
-                           "</dir>" % (name,name,id,icon)
-                except:
+                           "</dir>" % (name, name, id, icon)
+                except Exception:
                     pass
-    except:
+    except Exception:
         pass
 
     jenlist = JenList(xml)
@@ -163,11 +151,11 @@ def get_swiftstreamz(url):
 @route(mode='SwiftCat', args=["url"])
 def get_swiftstreamz_category(url):
     xml = ""
-    url = url.replace('swiftcategory/', '') # Strip our category tag off.
+    url = url.replace('swiftcategory/', '')  # Strip our category tag off.
     try:
         url = base_cat_url % (url)
-        headers = {'Authorization': 'Basic QFN3aWZ0MTEjOkBTd2lmdDExIw', 'User-Agent': User_Agent}
-        response = requests.get(url,headers=headers)
+        headers = {'Authorization': 'Basic @Swift11#:@Swift11#', 'User-Agent': User_Agent}
+        response = requests.get(url, headers=headers)
         if 'Erreur 503' in response.content:
             xml += "<dir>"\
                    "    <title>[B]System down for maintenance[/B]</title>"\
@@ -180,10 +168,10 @@ def get_swiftstreamz_category(url):
         else:
             response = response.json(strict=False)
             for a in response['LIVETV']:
-                if not 'm3u8' in a['channel_url']:
+                if 'm3u8' not in a['channel_url'] and '2ts' not in a['channel_url']:
                     continue
                 name = a['channel_title']
-                url  = a['channel_url']
+                url = a['channel_url']
                 icon = base_ico_url % (a['channel_thumbnail'])
                 desc = a['channel_desc']
                 xml += "<item>"\
@@ -193,8 +181,9 @@ def get_swiftstreamz_category(url):
                        "    </meta>"\
                        "    <swift>swiftplay/%s/%s</swift>"\
                        "    <thumbnail>%s</thumbnail>"\
-                       "</item>" % (name,desc,name,url,icon)
-    except:
+                       "</item>" % (name, desc, name, url, icon)
+    except Exception:
+        # xbmcgui.Dialog().textviewer('Whatevs', str(response))
         pass
 
     jenlist = JenList(xml)
@@ -203,17 +192,19 @@ def get_swiftstreamz_category(url):
 
 @route(mode='SwiftPlay', args=["url"])
 def get_swiftstreamz_play(url):
-    url = url.replace('swiftplay/', '') # Strip our category tag off.
-    tmp = url.split('/',1)
+    url = url.replace('swiftplay/', '')  # Strip our category tag off.
+    tmp = url.split('/', 1)
     title = tmp[0]
     url = tmp[1]
     try:
         repair = False
-        response = requests.get(base_dta_url,headers={'Authorization':'Basic U3dpZnRTdHJlYW16OkBTd2lmdFN0cmVhbXpA','User-Agent':User_Agent})
+        response = requests.get(
+            base_dta_url,
+            headers={'Authorization': 'Basic U3dpZnRTdHJlYW16OkBTd2lmdFN0cmVhbXpA', 'User-Agent': User_Agent})
         data = response.json(strict=False)
 
         if data['DATA'][0]['HelloUrl'] in url \
-        or data['DATA'][0]['HelloUrl1'] in url:
+                or data['DATA'][0]['HelloUrl1'] in url:
             auth_url = data['DATA'][0]['HelloLogin']
             auth_auth = tuple(data['DATA'][0]['PasswordHello'].split(':'))
         elif data['DATA'][0]['LiveTvUrl'] in url:
@@ -228,25 +219,31 @@ def get_swiftstreamz_play(url):
             auth_auth = tuple(data['DATA'][0]['Password'].split(':'))
             repair = True
 
-        r = requests.get(auth_url, headers = {'User-Agent': User_Agent}, auth=auth_auth, timeout=10)
+        r = requests.get(auth_url, headers={'User-Agent': User_Agent}, auth=auth_auth, timeout=10)
         auth_token = r.text.partition('=')[2]
 
         if repair:
-            auth_token = ''.join([auth_token[:-59], auth_token[-58:-47], auth_token[-46:-35], auth_token[-34:-23], auth_token[-22:-11], auth_token[-10:]])
+            auth_token = ''.join(
+                [auth_token[: -59],
+                 auth_token[-58: -47],
+                 auth_token[-46: -35],
+                 auth_token[-34: -23],
+                 auth_token[-22: -11],
+                 auth_token[-10:]])
 
-        if 'playlist.m3u8' in url:
-            url = url + '?wmsAuthSign=' + auth_token + '|User-Agent=123456'  
+        if 'playlist.m3u8' in url or '.2ts' in url:
+            url = url + '?wmsAuthSign=' + auth_token + '|User-Agent=123456'
             item = xbmcgui.ListItem(label=title, path=url, iconImage=addon_icon, thumbnailImage=addon_icon)
-            item.setInfo( type="Video", infoLabels={ "Title": title } )
+            item.setInfo(type="Video", infoLabels={"Title": title})
             import resolveurl
-            koding.Play_Video(url,showbusy=False,ignore_dp=True,item=item,resolver=resolveurl)        
-            #xbmc.executebuiltin("PlayMedia(%s)" % url)
+            koding.Play_Video(url, showbusy=False, ignore_dp=True, item=item, resolver=resolveurl)
+            # xbmc.executebuiltin("PlayMedia(%s)" % url)
         else:
             url = url + '|User-Agent=123456'
             item = xbmcgui.ListItem(label=title, path=url, iconImage=addon_icon, thumbnailImage=addon_icon)
-            item.setInfo( type="Video", infoLabels={ "Title": title } )
+            item.setInfo(type="Video", infoLabels={"Title": title})
             import resolveurl
-            koding.Play_Video(url,showbusy=False,ignore_dp=True,item=item,resolver=resolveurl)
-            #xbmc.executebuiltin("PlayMedia(%s)" % url)
-    except:
+            koding.Play_Video(url, showbusy=False, ignore_dp=True, item=item, resolver=resolveurl)
+            # xbmc.executebuiltin("PlayMedia(%s)" % url)
+    except Exception:
         xbmcgui.Dialog().ok('Stream', 'Unable to play stream')
